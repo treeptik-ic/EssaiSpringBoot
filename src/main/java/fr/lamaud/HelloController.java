9aef189bb2108a021ab103acbd919a83f914ce71
c.lamaud <c.lamaud@treeptik.fr>
2017-12-27 09:53:39 +0100

    Debut Tuto mkyong

--- src/main/java/fr/lamaud/HelloController.java
+++ src/main/java/fr/lamaud/HelloController.java
@@ -1,14 +1,23 @@
 package fr.lamaud;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-	@RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+	   // inject via application.properties
+		@Value("${welcome.message:test}")
+		private String message = "Hello World";
+
+		@RequestMapping("/")
+		public String welcome(Map<String, Object> model) {
+			model.put("message", this.message);
+			return "welcome";
+		}
 	
 }
